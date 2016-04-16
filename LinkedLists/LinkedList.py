5133da11d494c2c843343e11d3f95ec4cb06c53b
Aditi13 <sharmaa3@uci.edu>
2016-04-16 16:38:59 -0400

    python naming conventions

--- LinkedLists/LinkedList.py
+++ LinkedLists/LinkedList.py
@@ -1,38 +1,62 @@
+from types import new_class
 class Link:
-    def __init__(self,value):
+    def __init__(self, value):
         self.value = value
         self.next = None
-class List:
+class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
-    def isEmpty(self):
+    def is_empty(self):
         return not self.head
-    def addNodeFront(self,newValue):
-        newLink  = Link (newValue)
-        if self.isEmpty():
-            self.head = newLink
-            self.tail = newLink
+    def get_length(self):
+        length =  0
+        if not self.is_empty():
+            curr = self.head
+            while curr:
+                curr = curr.next
+                length +=1
+            return length
+        return length
+    def print_list (self):
+        if not self.is_empty():
+            curr = self.head
+            while curr:
+                print(curr.value,'-->', end = '')
+                curr = curr.next
         else:
-            newLink.next = self.head
-            self.head = newLink
-    def addNodeLast(self,newValue):
-        newLink = Link (newValue)
-        if self.isEmpty():
-            self.head = newLink
-            self.tail = newLink
+            print ('List empty')
+    def add_link_front(self, value):
+        new_link = Link(value)
+        if self.is_empty():
+            self.head = new_link
+            self.tail = new_link
         else:
-            self.tail.next = newLink
-            self.tail = newLink
-    def delNodeFront(self):
-        pass
-    def printList(self):
-        if not self.isEmpty():
-            currLink = self.head
-            while currLink:
-                print (currLink.value , '--> ' , end ='')
-                currLink = currLink.next
+            new_link.next = self.head
+            self.head = new_link
+    def add_link_last(self, value):
+        new_link = Link(value)
+        if self.is_empty():
+            self.head = new_link
+            self.tail = new_link
+        else:
+            self.tail.next = new_link
+            self.tail = new_link
+    def add_link_middle(self, value):
+        new_link = Link(value)
+        if self.is_empty():
+            self.tail = new_link
+            self.head = new_link
+        else:
+            slow_ptr = self.head
+            fast_ptr = self.head
+            while fast_ptr.next and fast_ptr.next.next:
+                fast_ptr = fast_ptr.next.next
+                slow_ptr = slow_ptr.next
+            new_link.next = slow_ptr.next
+            slow_ptr.next = new_link
+            
 
-      
-        
-        
+            
+            
+            
